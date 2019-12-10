5b1360fb0d4519c9050d919098b22f20e77e606e
Nitin Chaurasia <gs.nitin@gmail.com>
2019-12-09 23:05:11 -0500

    first commit

---
+++ src/main/java/com/recipe/recipespringapp/RecipeSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.recipe.recipespringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recipe/recipespringapp/RecipeSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipe.recipespringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
